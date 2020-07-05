f65be9fcebfb2a59e1e0550e3e24012aaa4415df
sidkcr <sidkcr@gmail.com>
2020-07-06 00:02:42 +0530

    Method to Add cities

--- blood-plus/services/BloodPlus.API/Repositories/CityRepository.cs
+++ blood-plus/services/BloodPlus.API/Repositories/CityRepository.cs
@@ -10,12 +10,31 @@ namespace BloodPlus.API.Repositories
     public interface ICityRepository
     {
         Task<IEnumerable<City>> GetByState(int stateId);
+        Task Add(IEnumerable<(string city, string state)> data);
     }
 
     public class CityRepository : BaseRepository, ICityRepository
     {
-        public CityRepository(IBloodPlusDatabaseContext dbContext) : base(dbContext)
+        private readonly IStateRepository _stateRepository;
+        public CityRepository(
+            BloodPlusDatabaseContext dbContext,
+            IStateRepository stateRepository) : base(dbContext)
         {
+            _stateRepository = stateRepository;
+        }
+
+        public async Task Add(IEnumerable<(string city, string state)> data)
+        {
+            await _stateRepository.Add(data.Select(x => x.state).Distinct());
+
+            var cities =
+                from d in data.Distinct()
+                join s in DbContext.States on d.state equals s.Name
+                where !DbContext.Cities.Any(x => x.Name.ToLower() == d.city.ToLower())
+                select new City {Name = d.city, StateId = s.Id};
+
+            await DbContext.Cities.AddRangeAsync(cities);
+            await DbContext.SaveChangesAsync();
         }
 
         public async Task<IEnumerable<City>> GetByState(int stateId)
